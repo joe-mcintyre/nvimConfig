74d59fc2f12883fd539087a2f980e38a52652148
Joe McIntyre <146065116+joe-mcintyre@users.noreply.github.com>
2024-11-16 19:12:47 +1100

    Delete lua directory

--- lua/configgy/init.lua
+++
@@ -1,2 +0,0 @@
-require("configgy.remap")
-require("configgy.set")
--- lua/configgy/remap.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-a>", "ggVG")
-print("loaded")
